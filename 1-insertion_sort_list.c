10e81b6abb2e738f4bf35e6bd0781c3a2222f0b4
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-20 01:51:24 +0300

    trying

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,71 +1,42 @@
 #include "sort.h"
 
-listint_t *change_no(listint_t **tmp);
-
 /**
- * insertion_sort_list - sorta dlinked list
- * @list: dlinked list
- *
- * Return: none
+ * insertion_sort_list - sort linked list using insertion method
+ * @list: input linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp = NULL, *t2 = NULL;
+	listint_t *current, *holder, *node_holder;
+	int flag;
 
-	if (!*list)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
-	tmp = *list;
-
-	while (tmp && tmp->next)
+	current = (*list)->next;
+	while (current != NULL)
 	{
-		if (tmp->n > tmp->next->n)
+		holder = current;
+		while (holder->prev != NULL)
 		{
-			tmp = change_no(&tmp);
-			print_list(*list);
-			t2 = tmp;
-		}
-
-		if (t2 && t2->prev)
-		{
-			while (t2 && t2->prev)
+			flag = 0;
+			if (holder->n < holder->prev->n)
 			{
-				if (t2->prev && t2->prev->n > t2->n)
-				{
-					t2 = t2->prev;
-					t2 = change_no(&t2);
-					if (t2->prev == NULL)
-						*list = tmp;
-					print_list(*list);
-				}
-				t2 = t2->prev;
+				if (holder->prev->prev != NULL)
+					holder->prev->prev->next = holder;
+				holder->prev->next = holder->next;
+				node_holder = holder->prev->prev;
+				holder->prev->prev = holder;
+				if (holder->next != NULL)
+					holder->next->prev = holder->prev;
+				holder->next = holder->prev;
+				holder->prev = node_holder;
+				flag = 1;
+				if (holder->prev == NULL)
+					*list = holder;
+				print_list(*list);
 			}
+			if (flag == 0)
+				holder = holder->prev;
 		}
-		tmp = tmp->next;
+		current = current->next;
 	}
-}
-
-/**
- * change_no - change nodes postion
- * @tmp: temporal node
- *
- * Return: changed node position
- */
-listint_t *change_no(listint_t **tmp)
-{
-	listint_t *t = NULL, *t_n = NULL;
-
-	t = *tmp;
-	t_n = (*tmp)->next;
-
-	if (t_n->next)
-		t_n->next->prev = t;
-	if (t->prev)
-		t->prev->next = t_n;
-	t_n->prev = t->prev;
-	t->next = t_n->next;
-	t->prev = t_n;
-	t_n->next = t;
-
-	return (t);
 }
